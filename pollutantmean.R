5231023165fa25e583f576300622e47bbe200d57
AliceAN <drakunyili@gmail.com>
2014-11-16 14:31:43 -0500

    Revert "Functions for programming assingment 1"
    This reverts commit 4ac95b9c7d83677637e207662a7544273b6bdab2.

--- pollutantmean.R
+++
@@ -1,8 +0,0 @@
-pollutantmean<-function(directory,pollutant,id=1:332){
-                
-        filenames <- list.files(directory, full.name=TRUE)
-        selectedfiles<-filenames[id]
-        data<-lapply(selectedfiles, read.csv)
-        output <- do.call(rbind, data)
-        mean(output[,pollutant], na.rm=TRUE)
-}
